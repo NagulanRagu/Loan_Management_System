668b884de33fe1414d4b29d15dba1b4115cd5a87
NagulanRagu <runagulan88@gmail.com>
2022-12-29 10:50:41 +0530

    Random Commit

---
+++ Authentication-Microservice/src/main/java/com/lms/AM/AuthenticationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lms.AM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Authentication-Microservice/src/test/java/com/lms/AM/AuthenticationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.AM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
