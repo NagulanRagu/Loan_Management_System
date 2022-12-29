4b439d0cd5e4bea38fb22c8eba2f6612ed75f354
NagulanRagu <runagulan88@gmail.com>
2022-12-29 10:25:14 +0530

    Change Commit

---
+++ Registration-Information-Microservice/src/main/java/com/lms/RI/RegistrationInformationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lms.RI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationInformationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationInformationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Registration-Information-Microservice/src/main/java/com/lms/RI/Service/LoanRegisteredService.java
@@ -0,0 +1,67 @@
+package com.lms.RI.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lms.RI.Model.LoanRegistered;
+import com.lms.RI.Repository.LoanRegisteredRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class LoanRegisteredService {
+    
+    @Autowired
+    LoanRegisteredRepository loanRegisteredRepository;
+
+    public List<LoanRegistered> getAllDetails() throws NullPointerException {
+
+        log.info("Getting all Details from Database.");
+        if(loanRegisteredRepository.findAll().isEmpty()) {
+            log.error("No Details is found in Database.");
+            throw new NullPointerException();
+        }else {
+            log.info("All Details are fetched.");
+            return loanRegisteredRepository.findAll();
+        }
+    }
+
+    public LoanRegistered saveDetails(LoanRegistered loanRegistered) {
+
+        log.info("Saving the Details {} to the Database.", loanRegistered);
+        return loanRegisteredRepository.save(loanRegistered);
+    }
+
+    public void deleteDetails(int id) {
+
+        log.info("Deleting the Details from Database for Id: {}", id);
+        loanRegisteredRepository.deleteById(id);
+    }
+
+    public List<LoanRegistered> getByLoanNo(String loanNo) throws IllegalArgumentException {
+
+        log.info("Getting Details by Loan Number: {}", loanNo);
+        if(loanRegisteredRepository.findByLoanNo(loanNo).isEmpty()) {
+            log.error("No Details found for Loan Number: {}", loanNo);
+            return loanRegisteredRepository.findByLoanNo(loanNo);
+        }else {
+            log.info("All Details are found for Loan Number: {}", loanNo);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<LoanRegistered> getByBorrowerId(int id) throws IllegalArgumentException {
+
+        log.info("Getting Details by Borrower Id: {}", id);
+        if(loanRegisteredRepository.findByBorrowerId(id).isEmpty()) {
+            log.error("No Details found for Borrower Id: {}", id);
+            return loanRegisteredRepository.findByBorrowerId(id);
+        }else {
+            log.info("All Details are found for Borrower Id: {}", id);
+            throw new IllegalArgumentException();
+        }
+    }
+}
