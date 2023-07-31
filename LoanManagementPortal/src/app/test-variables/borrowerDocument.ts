7cd28593040df40f4079b1d435aa5a38be4bbabe
NagulanRagu <runagulan88@gmail.com>
2023-07-31 17:22:07 +0530

    final commit

---
+++ LoanManagementPortal/src/app/test-variables/borrowerDocument.ts
@@ -0,0 +1,11 @@
+import { ResponseFile } from "../model/response-file";
+
+export const responseFile: ResponseFile[] = [
+    {
+        "fileName": "Passport.pdf",
+        "fileDetail": "AadhaarCard",
+        "fileType": "application/pdf",
+        "fileUri": "http://localhost:8084/downloadFile/1",
+        "fileSize": 393228
+    }
+]
